f35ca0dedf13caf024ca6ed132656c12ace2582c
FADUMA IBRAHIM AHMED <1911144@stu.kau.edu.sa>
2023-03-20 21:33:20 +0300

    added my project

---
+++ JavaScript1.js
@@ -0,0 +1,15 @@
+﻿var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
